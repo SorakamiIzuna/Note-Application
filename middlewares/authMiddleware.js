fe36db7d5f9ee9a20c6167c76d540718b0910842
22120231 <22120231@student.hcmus.edu.vn>
2025-01-24 18:42:45 +0700

    fix redirect trong authenticateToken

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const token = req.cookies.token; // Token từ cookie
-    if (!token) return res.redirect('/login'); // Chưa đăng nhập
+    if (!token) return res.redirect('/auth/login'); // Chưa đăng nhập
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.redirect('/login'); // Token không hợp lệ
+        if (err) return res.redirect('/auth/login'); // Token không hợp lệ
         req.user = user; // Lưu thông tin người dùng
         next();
     });
